42b3ddb815b704e3ebaa9a565c6ced643aa47a19
Peter Kimberley <p3t3rk1m80@gmail.com>
2025-10-15 22:21:56 +0100

    Updated the string on line 10 to ask what percent of a tip would they like to leave. Also chnaged the f string on line 20.

--- TipCalculator.py
+++ TipCalculator.py
@@ -7,7 +7,7 @@ splitBetweenHowMany = int(input\
                           ("How many people are splitting the bill? "))
 
 
-tip = float(input("How much of a tip would you like to leave? "))
+tip = float(input("What % tip would you like to leave? "))
 
 
 """This is the actual calculation for the total to pay
@@ -17,4 +17,4 @@ amountToPayPerPerson = (totlaOfTheBill /splitBetweenHowMany * tip)
 """This will calculate the price too 2 decimal places"""
 amountToPayPerPerson = float(amountToPayPerPerson)
 
-print("The total amount to pay is " + str(round(amountToPayPerPerson, 2)))
+print(f"The total amount to pay is: + ${amountToPayPerPerson}")
